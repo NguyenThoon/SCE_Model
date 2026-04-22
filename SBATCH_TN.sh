ce37226c67d0ab4ee6f0f5432dde3e7a08d32862
Thoon Nguyen <thoon.nguyen@email.ucr.edu>
2026-04-22 12:26:39 -0700

    add SBATCH_TN.sh

---
+++ SBATCH_TN.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=60G
+#SBATCH --time=0-2:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --job-name="TN_job_test"
+#SBATCH -p short_gpu
+#SBATCH --gpus-per-task=1
+
+hostname
+echo "Time: $(date)"
+source /etc/profile.d/modules.sh
+
+module purge
+module load cmake
+module load cuda/11.4
+module load gcc/9.2.1
+module load extra
+module load openmpi/4.1.2_slurm-21.08.5
+
+# go to the repo root
+cd /rhome/tnguy1340/sce_test/SCE_Model || exit 1
+
+# optional: build
+./build_EpiScale_Signal.sh build || exit 1
+
+# run
+./bin/runDiscSimulation_M
