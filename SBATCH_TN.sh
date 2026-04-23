92dfea795973ebf6f2b3c1498ae1c0e017e2f5e3
Thoon Nguyen <thoon.nguyen@email.ucr.edu>
2026-04-23 00:32:30 -0700

    Debug

--- SBATCH_TN.sh
+++ SBATCH_TN.sh
@@ -21,11 +21,16 @@ module load gcc/9.2.1
 module load extra
 module load openmpi/4.1.2_slurm-21.08.5
 
-# go to the repo root
 cd /rhome/tnguy1340/sce_test/SCE_Model || exit 1
 
-# optional: build
+echo "PWD: $(pwd)"
+ls -l ./bin/runDiscSimulation_M
+strings ./bin/runDiscSimulation_M | grep "DEBUG: tissueCenter size" || exit 1
+
 ./build_EpiScale_Signal.sh build || exit 1
 
-# run
+echo "After build:"
+ls -l ./bin/runDiscSimulation_M
+strings ./bin/runDiscSimulation_M | grep "DEBUG: tissueCenter size" || exit 1
+
 ./bin/runDiscSimulation_M
